77cb14fff03bcb2063a075a6c52e6f3ac18ad430
unknown <jeniphiloesw@gmail.com>
2022-02-14 14:09:28 +0530

    calculator and for loops

---
+++ calculator/precedence.js
@@ -0,0 +1,10 @@
+var totalPrice = 12.00;
+var taxRate = .1;
+var retailPrice = (1+taxRate) + totalPrice * 2/.5  ;
+console.log(retailPrice);
+
+console.log(3 + 10 * 2); 
+console.log((3 + 10) * 2);
+console.log
+
+
